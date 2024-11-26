44b876a80faae43dda152fe8a3446546442af2b8
Shahzaib-Anees <159277068+Shahzaib-Anees@users.noreply.github.com>
2024-11-27 00:01:51 +0500

    Adding Compete Backend

---
+++ index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./src/db/index.js";
+import todoRoutes from "./src/routes/routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/v1", todoRoutes);
+
+(async () => {
+  try {
+    const res = await connectDB();
+    console.log(res);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
